c36717ed30b9ad2698724b3b9c7007c5a937b1f4
Muhammad Aqeel <intern2@shayansolutions.com>
2018-07-30 08:24:32 +0500

    first commit

---
+++ src/app/message.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  messages:String[] =[];
+
+  constructor() { 
+    this.messages = [];
+  }
+
+  add(message: string):void
+  {
+    this.messages.push(message);
+  }
+
+  clear():void
+  {
+    this.messages = [];
+  }
+}
